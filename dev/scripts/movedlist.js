fbe4e333cda8781669fb3c68d19873dc0fc33dfa
Lindsay Woods <lindsaywoods@live.ca>
2018-02-27 09:51:21 -0500

    spacing on button

--- dev/scripts/movedlist.js
+++ dev/scripts/movedlist.js
@@ -4,12 +4,12 @@ const MovedListItem = (props) => {
     return (
         <div className="masterlist">
             <li>{props.data} 
-            <button className="trashbutton" onClick={() => props.remove(props.movedItemIndex)}>
-                <img src="./public/styles/assets/smileytrash.svg" alt="delete item" /> 
-            </button>
-            <button className="strikebutton" onClick={() => props.strikethrough(props.itemIndex)}>
-            <img src="./public/styles/assets/check2.svg" alt="mark to do completed"/>
-            </button>
+                <button className="trashbutton" onClick={() => props.remove(props.movedItemIndex)}>
+                    <img src="./public/styles/assets/smileytrash.svg" alt="delete item" /> 
+                </button>
+                <button className="strikebutton" onClick={() => props.strikethrough(props.itemIndex)}>
+                <img src="./public/styles/assets/check2.svg" alt="mark to do completed"/>
+                </button>
             </li>
         
         </div>
